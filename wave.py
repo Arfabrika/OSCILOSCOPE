77546bd4f76c4d495ddf107a2b1e06666716c56b
IlyaBozh <86633291+IlyaBozh@users.noreply.github.com>
2022-09-29 16:31:12 +0300

    Update wave.py

--- wave.py
+++ wave.py
@@ -37,39 +37,40 @@ def generate_square_wave(amplitude, freq, sample_rate, duration):
     return x, y
 
 
-# def mod_generate_sine_wave(freq, sample_rate, duration):
-#     x = np.linspace(0, duration, sample_rate * duration, endpoint=False)
-#     frequencies = x * freq
-#     y = np.sin(frequencies * (2 * np.pi))
-#     return x, y
-#
-#
-# def mod_generate_cosine_wave(freq, sample_rate, duration):
-#     x = np.linspace(0, duration, sample_rate * duration, endpoint=False)
-#     frequencies = x * freq
-#     y = np.cos(frequencies * (2 * np.pi))
-#     return x, y
-#
-#
-# def mod_generate_triangle_wave(freq, sample_rate, duration):
-#     x = np.linspace(0, duration, sample_rate * duration, endpoint=False)
-#     frequencies = x * freq
-#     y = signal.sawtooth(frequencies * (2 * np.pi), 0.5)
-#     return x, y
-#
-#
-# def mod_generate_sawtooth_wave(freq, sample_rate, duration):
-#     x = np.linspace(0, duration, sample_rate * duration, endpoint=False)
-#     frequencies = x * freq
-#     y = signal.sawtooth(frequencies * (2 * np.pi), 1)
-#     return x, y
-#
-#
-# def mod_generate_square_wave(freq, sample_rate, duration):
-#     x = np.linspace(0, duration, sample_rate * duration, endpoint=False)
-#     frequencies = x * freq
-#     y = signal.square(frequencies * (2 * np.pi))
-#     return x, y
+def mod_generate_sine_wave(freq, sample_rate, duration):
+    x = np.linspace(0, duration, sample_rate * duration, endpoint=False)
+    frequencies = x * freq
+    y = np.sin(frequencies * (2 * np.pi))
+    return x, y
+
+
+def mod_generate_cosine_wave(freq, sample_rate, duration):
+    x = np.linspace(0, duration, sample_rate * duration, endpoint=False)
+    frequencies = x * freq
+    y = np.cos(frequencies * (2 * np.pi))
+    return x, y
+
+
+def mod_generate_triangle_wave(freq, sample_rate, duration):
+    x = np.linspace(0, duration, sample_rate * duration, endpoint=False)
+    frequencies = x * freq
+    y = signal.sawtooth(frequencies * (2 * np.pi), 0.5)
+    return x, y
+
+
+def mod_generate_sawtooth_wave(freq, sample_rate, duration):
+    x = np.linspace(0, duration, sample_rate * duration, endpoint=False)
+    frequencies = x * freq
+    y = signal.sawtooth(frequencies * (2 * np.pi), 1)
+    return x, y
+
+
+def mod_generate_square_wave(freq, sample_rate, duration):
+    x = np.linspace(0, duration, sample_rate * duration, endpoint=False)
+    frequencies = x * freq
+    y = signal.square(frequencies * (2 * np.pi))
+    return x, y
+
 
 def modulating (fs_frequency, fs_sample_rate, fs_duration, ss_amplitude, ss_frequency, fs_amplitude):
     x = np.linspace(0, fs_duration, fs_sample_rate * fs_duration, endpoint=False)
