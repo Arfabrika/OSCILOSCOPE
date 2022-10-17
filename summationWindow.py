d0cce5772033beb59ec25b5fa6c5524f441224f6
IlyaBozh <86633291+IlyaBozh@users.noreply.github.com>
2022-10-17 15:54:03 +0300

    Update summationWindow.py

--- summationWindow.py
+++ summationWindow.py
@@ -7,15 +7,20 @@ from PySide6.QtWidgets import (
     QPushButton
 )
 
+from SignalPlotWidget import SignalPlotWidget
+from signalData import signalData, signalDataArray
+
 class SummationWindow(QWidget):
-    def __init__(self, signalDataArray, parent=None):
+    def __init__(self, DataArray, parent=None):
         super().__init__(parent)
-        self.signalDataArray = signalDataArray
+        self.signalDataArray = DataArray
+        self.signalsOnPlot = signalDataArray([])
         
         self.fs_signals_label = QLabel('Основной сигнал')
         self.fs_signals_list = QComboBox(self)
         #self.signals_list.addItems(signal_types)
         self.fs_signals_label.setBuddy(self.fs_signals_list)
+        self.fs_signals_list.currentIndexChanged.connect(self.showSignalInfo_fs)
         # self.fs_signals_list.currentIndexChanged.connect(self.showSignalInfo_fs)
 
         fs_signals_layout = QHBoxLayout()
@@ -62,7 +67,6 @@ class SummationWindow(QWidget):
         fs_duration_layout.addWidget(self.fs_duration_label)
         fs_duration_layout.addWidget(self.fs_duration_spin)
 
-        self.summation_button = QPushButton('Create plots')
 
         fs_signal = QVBoxLayout()
         fs_signal.addLayout(fs_signals_layout)
@@ -72,6 +76,13 @@ class SummationWindow(QWidget):
         fs_signal.addLayout(fs_sample_rate_layout)
         fs_signal.addLayout(fs_duration_layout)
 
+        self.ok_button = QPushButton('Add signal')
+        self.ok_button.clicked.connect(self.button_clicked)
+        self.step_out_button = QPushButton('Step back')
+        self.step_out_button.clicked.connect(self.step_back)
+
+        self.plot = SignalPlotWidget()
+
         signal_layout = QHBoxLayout()
 
         signal_layout.addLayout(fs_signal)
@@ -79,6 +90,9 @@ class SummationWindow(QWidget):
         main_layout = QVBoxLayout()
 
         main_layout.addLayout(signal_layout)
+        main_layout.addWidget(self.ok_button)
+        main_layout.addWidget(self.step_out_button)
+        main_layout.addWidget(self.plot)
 
         self.setLayout(main_layout)
         self.setSignals()
@@ -90,3 +104,32 @@ class SummationWindow(QWidget):
         else:
             for i in range(len(data)):
                 self.fs_signals_list.addItem('Signal ' + str(i + 1))
+
+    
+    def button_clicked(self):
+        curSignal_fs = self.signalDataArray.getSignalByIndex(self.fs_signals_list.currentIndex()).getData() 
+       
+        if self.signalsOnPlot.getArraySize() == 0:
+            self.plot.plot(curSignal_fs[0], curSignal_fs[1], curSignal_fs[2], curSignal_fs[3], curSignal_fs[4], 1, 1)
+            self.signalsOnPlot.appendSignal(signalData(curSignal_fs[0], curSignal_fs[1], curSignal_fs[2], curSignal_fs[3], curSignal_fs[4], False))
+        elif self.signalsOnPlot.getArraySize() == 1:
+            ss_signal = self.signalsOnPlot.getSignalByIndex(self.signalsOnPlot.getArraySize() - 1)
+            self.plot.polyharmonic(curSignal_fs[0], curSignal_fs[1], curSignal_fs[2], curSignal_fs[3], curSignal_fs[4], ss_signal.getSignaType(), ss_signal.getAmplitude(), ss_signal.getFrequency(), ss_signal.getSampleRate(), ss_signal.getDuration())
+            self.signalsOnPlot.appendSignal(signalData(curSignal_fs[0], curSignal_fs[1], curSignal_fs[2], curSignal_fs[3], curSignal_fs[4], False))
+        else:
+            self.plot.polyharmonic(curSignal_fs[0], curSignal_fs[1], curSignal_fs[2], curSignal_fs[3], curSignal_fs[4])
+    
+    def showSignalInfo_fs(self):
+       curSignal_fs = self.signalDataArray.getSignalByIndex(self.fs_signals_list.currentIndex()).getData() 
+
+       self.fs_amplitude_spin.setText(str(curSignal_fs[1]))
+       self.fs_duration_spin.setText(str(curSignal_fs[4]))
+       self.fs_frequency_spin.setText(str(curSignal_fs[2]))
+       self.fs_sample_rate_spin.setText(str(curSignal_fs[3]))
+       self.fs_signal_form_combo.setText(curSignal_fs[0])
+    
+    def step_back(self):
+        self.plot.remove_last_points()
+       # bashkoff
+
+
