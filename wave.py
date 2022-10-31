93261aad935f57f4cc7fb1bd8d155fb544b01de3
IlyaBozh <86633291+IlyaBozh@users.noreply.github.com>
2022-10-31 16:29:08 +0300

    Update wave.py

--- wave.py
+++ wave.py
@@ -73,7 +73,7 @@ def mod_generate_square_wave(freq, sample_rate, duration):
 
 
 def modulating (fs_frequency, fs_sample_rate, fs_duration, ss_amplitude, ss_frequency, fs_amplitude):
-    x = np.linspace(-fs_duration, fs_duration, fs_sample_rate * int(10 / fs_duration) if 10 / fs_duration > 1 else int(fs_sample_rate * fs_duration), endpoint=False)
+    x = np.linspace(0, fs_duration, fs_sample_rate * fs_duration, endpoint=False)
     y = []
 
     t1 = (2 * np.pi) / (1 / ss_frequency)
@@ -88,15 +88,25 @@ def specter_modulating(fs_frequency, fs_sample_rate, fs_duration, ss_amplitude, 
     x = np.linspace(0, fs_duration, fs_sample_rate * fs_duration, endpoint=False)
     y = []
 
+    t0 = (2 * np.pi) / (1 / fs_frequency)
     t1 = (2 * np.pi) / (1 / ss_frequency)
-    t2 = (2 * np.pi) / (1 / fs_frequency)
 
-    for point in x:
-       y.append(0)
-    index = len(x) // 2
+    y.append(abs((fs_amplitude * ((ss_amplitude / fs_amplitude) / 2)) * np.cos((t0 - t1) * x.max())))
+    y.append(abs(fs_amplitude * np.cos(t0 * x.max())))
+    y.append(abs((fs_amplitude * ((ss_amplitude / fs_amplitude) / 2)) * np.cos((t0 + t1) * x.max())))
+   
+    x = []
 
-    y[index] = fs_amplitude * np.cos(t2 * point)
-    y[index + len(x) // 6] = (fs_amplitude * (ss_amplitude / fs_amplitude) / 2) * np.cos((t2 + t1) * point)
-    y[index - len(x) // 6] = (fs_amplitude * (ss_amplitude / fs_amplitude) / 2) * np.cos((t2 - t1) * point)
+    x.append(t0-t1)
+    x.append(t0)
+    x.append(t0+t1)
     return x, y
 
+def freq_modulating(fs_frequency, fs_sample_rate, fs_duration, ss_amplitude, ss_frequency, fs_amplitude):
+    x = np.linspace(0, fs_duration - 3, fs_sample_rate * fs_duration * 5, endpoint=False)
+    y = []
+    
+    for point in x:
+        #y.append(np.cos(400 * point + 10 * np.sin(25 * point)))
+        y.append(fs_amplitude * np.cos(ss_frequency * 2 * np.pi * point + 10 * np.sin(fs_frequency * point * 2 * np.pi)))
+    return x, y
