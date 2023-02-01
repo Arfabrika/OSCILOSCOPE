8d5e6c3a6a440b1b906cccc1413f52fb0cdfea95
IlyaBozh <86633291+IlyaBozh@users.noreply.github.com>
2023-02-01 22:41:48 +0300

    Create PlotWindow.py

---
+++ PlotWindow.py
@@ -0,0 +1,27 @@
+from PySide6.QtWidgets import (
+    QWidget,
+    QLabel,
+    QComboBox,
+    QVBoxLayout,
+    QHBoxLayout,
+    QPushButton,
+    QDial,
+    QCheckBox
+)
+
+from SignalPlotWidget import SignalPlotWidget
+
+class PlotWindow(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.signal_plot = SignalPlotWidget()
+
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(self.signal_plot)
+
+        self.setLayout(main_layout)
+
+    def plot_graph(self, signalDataArray):
+        self.signal_plot.clear()
+
+        self.signal_plot.plot(signalDataArray[0], signalDataArray[2], signalDataArray[1], signalDataArray[3],flag=0)
